e7e8be7693844f20b0d87435e1d772f730dad787
Changming Sun <chasun@microsoft.com>
2021-08-15 14:40:48 -0700

    update

--- tools/ci_build/github/linux/cibw_before_build_x86_64.sh
+++ tools/ci_build/github/linux/cibw_before_build_x86_64.sh
@@ -2,5 +2,7 @@
 #This script runs in top src dir
 set -e -x
 rm -rf Release
-python3 tools/ci_build/build.py --build_dir=`pwd` --config Release --update --build  --skip_submodule_sync --parallel --enable_pybind --enable_lto
-cp setup.py Release
+python3 tools/ci_build/build.py --build_dir=`pwd` --config Release --update --skip_submodule_sync --parallel --enable_pybind --enable_lto
+cd Release
+make -j$(nproc) onnxruntime_pybind11_state
+cp ../setup.py .
--- tools/ci_build/github/linux/cibw_before_build_x86_64_gpu.sh
+++ tools/ci_build/github/linux/cibw_before_build_x86_64_gpu.sh
@@ -2,5 +2,7 @@
 #This script runs in top src dir
 set -e -x
 rm -rf Release
-python3 tools/ci_build/build.py --build_dir=`pwd` --config Release --update --build --skip_submodule_sync --parallel --enable_pybind --use_cuda --cuda_version=11.1 --cuda_home=/usr/local/cuda-11.1 --cudnn_home=/usr/local/cuda-11.1 --cmake_extra_defines CMAKE_CUDA_HOST_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/cc 'CMAKE_CUDA_ARCHITECTURES=37;50;52;60;61;70;75;80'
-cp setup.py Release
+python3 tools/ci_build/build.py --build_dir=`pwd` --config Release --update --skip_submodule_sync --parallel --enable_pybind --use_cuda --cuda_version=11.1 --cuda_home=/usr/local/cuda-11.1 --cudnn_home=/usr/local/cuda-11.1 --cmake_extra_defines CMAKE_CUDA_HOST_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/cc 'CMAKE_CUDA_ARCHITECTURES=37;50;52;60;61;70;75;80'
+cd Release
+make -j$(nproc) onnxruntime_pybind11_state
+cp ../setup.py .
