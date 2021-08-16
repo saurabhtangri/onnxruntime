07ceb5876bfb70a8f3985e57a0df2364f85676d1
Changming Sun <chasun@microsoft.com>
2021-08-16 02:28:37 -0700

    update

--- tools/ci_build/github/linux/cibw_before_build_aarch64.sh
+++ tools/ci_build/github/linux/cibw_before_build_aarch64.sh
@@ -2,7 +2,7 @@
 #This script runs in top src dir
 set -e -x
 rm -rf Release
-python3 tools/ci_build/build.py --build_dir=`pwd` --config Release --update --skip_submodule_sync --parallel --enable_pybind --enable_lto
+python3 tools/ci_build/build.py --build_dir=`pwd` --config Release --update --skip_submodule_sync --parallel --enable_pybind
 cd Release
 make -j$(nproc) onnxruntime_pybind11_state
 cp ../setup.py .
